4cedfe86c579be50f598d8bf2c41b5766273d108
Tim Sebastian <tim.sebastian@gmail.com>
2016-10-19 16:35:40 +0200

    get relative path based on process cwd to report filename in vscode

--- formatters/vscodeFormatter.ts
+++ formatters/vscodeFormatter.ts
@@ -1,9 +1,10 @@
+import * as path from 'path';
 import * as ts from 'typescript';
 import * as Lint from 'tslint/lib/lint';
 
 export class Formatter extends Lint.Formatters.AbstractFormatter {
   private formatFailure(failure: Lint.RuleFailure): string {
-    const fileName = failure.getFileName();
+    const fileName = path.relative(process.cwd(), failure.getFileName());
     const {line, character} = failure.getStartPosition().getLineAndCharacter();
     const message = failure.getFailure();
     const ruleName = failure.getRuleName();
