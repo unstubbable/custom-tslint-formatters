0b0eb0c695aefab5e66412848dfd0781fa6b9a09
Hendrik Liebau <mail@hendrik-liebau.de>
2018-02-26 12:00:32 +0100

    feat: sort failures by start position

--- formatters/groupedFormatter.ts
+++ formatters/groupedFormatter.ts
@@ -1,7 +1,7 @@
 import * as Lint from 'tslint';
 import chalk from 'chalk';
 
-interface IGroupedFailures {
+interface GroupedFailures {
   [fileName: string]: Lint.RuleFailure[];
 }
 
@@ -16,20 +16,42 @@ export class Formatter extends Lint.Formatters.AbstractFormatter {
     return `  ${positionColor(position)} ${message} ${chalk.dim(ruleName)}`;
   }
 
-  private groupByFile(failures: Lint.RuleFailure[]): IGroupedFailures {
+  private groupByFile(failures: Lint.RuleFailure[]): GroupedFailures {
     return failures.reduce(
-      (groups: IGroupedFailures, failure: Lint.RuleFailure) => {
+      (groups: GroupedFailures, failure: Lint.RuleFailure) => {
         const fileName = failure.getFileName();
         const fileFailures = groups[fileName] || [];
         groups[fileName] = [...fileFailures, failure];
         return groups;
       },
-      {} as IGroupedFailures
+      {} as GroupedFailures
     );
   }
 
+  protected sortFailures(failures: Lint.RuleFailure[]): Lint.RuleFailure[] {
+    return failures.sort((a, b) => {
+      const {
+        line: lineA,
+        character: characterA,
+      } = a.getStartPosition().getLineAndCharacter();
+      const {
+        line: lineB,
+        character: characterB,
+      } = b.getStartPosition().getLineAndCharacter();
+      const filenameA = a.getFileName();
+      const filenameB = b.getFileName();
+
+      if (filenameA === filenameB) {
+        return lineA === lineB ? characterA - characterB : lineA - lineB;
+      }
+
+      return filenameA < filenameB ? -1 : filenameA > filenameB ? 1 : 0;
+    });
+  }
+
   public format(failures: Lint.RuleFailure[]): string {
-    const failuresByFile = this.groupByFile(failures);
+    const failuresByFile = this.groupByFile(this.sortFailures(failures));
+
     return Object.keys(failuresByFile)
       .reduce((lines: string[], fileName: string) => {
         lines.push(chalk.underline.green(fileName));
